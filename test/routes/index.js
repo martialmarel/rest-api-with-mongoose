a1a3fc2acf85d2a7e3d1ba5a7c00f179c3b96374
Martial <martial@marels.fr>
2018-03-04 23:24:18 +0100

    add test/routes/index

---
+++ test/routes/index.js
@@ -0,0 +1,18 @@
+/* global request:false, expect:false */
+
+describe('Routes: Accounts', () => {
+	describe('GET /', () => {
+		it('returns the API status', done => {
+			request.get('/')
+				.expect(200)
+				.end((err, res) => {
+
+					const nowExpected = (new Date().toISOString()).split('.')[0];
+					expect(res.body).to.have.property('now');
+					expect(res.body).to.have.property('uptime');
+					expect(res.body.now).to.string(nowExpected);
+					done(err);
+				});
+		});
+	});
+});
